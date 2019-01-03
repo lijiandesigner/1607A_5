e3a69aa1b4ff3fc813772151147ef9d8ef5b1af1
1149472629@qq.com <1149472629@qq.com>
2019-01-03 21:14:01 +0800

    布局页更改

---
+++ MVC/Controllers/MUBanController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class MUBanController : Controller
+    {
+        // GET: MUBan
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: MUBan
+        public ActionResult list()
+        {
+            return View();
+        }
+    }
+}
