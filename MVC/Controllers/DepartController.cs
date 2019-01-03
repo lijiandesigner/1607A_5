6070ce8eb556888d803fa59c9605f18e450a2b02
wyh327614 <1046609085@qq.com>
2019-01-03 21:03:35 +0800

    更改

---
+++ MVC/Controllers/DepartController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using Model;
+
+namespace MVC.Controllers
+{
+    public class DepartController : Controller
+    {
+        // GET: Depart
+        DepartBLL BLL = new DepartBLL();
+        public ActionResult Index()
+        {
+            List<Depart> list = BLL.GetList();
+            return View(list);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Depart depart)
+        {
+            depart.CreateTime = DateTime.Now.ToString("yyyy年MM月dd日 HH时mm分 dddd");
+            int result = BLL.Add(depart);
+            if(result>0)
+            {
+                Response.Write("<script>alert('添加部门成功')</script>");
+            }
+            return Redirect("/Depart/Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            int result = BLL.Del(id);
+            if(result>0)
+            {
+                Response.Write("<script>alert('删除部门成功')</script>");
+            }
+            return Redirect("/Depart/Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            Depart depart = BLL.GetT(id);
+            return View(depart);
+        }
+        [HttpPost]
+        public ActionResult Edit(Depart depart)
+        {
+            depart.CreateTime = DateTime.Now.ToString("yyyy年MM月dd日 HH时mm分 dddd");
+            int result = BLL.Upt(depart);
+            if (result > 0)
+            {
+                Response.Write("<script>alert('修改部门成功')</script>");
+            }
+            return Redirect("/Depart/Index");
+        }
+    }
+}
--- MVC/Controllers/LoginController.cs
+++ MVC/Controllers/LoginController.cs
@@ -19,11 +19,10 @@ namespace MVC.Controllers
         [HttpPost]
         public ActionResult Index(string username, string password)
         {
-            List<Staff> list = BLL.GetList();
-            List<Staff> result = list.Where(s => s.StaffName == username && s.StaffCard.Substring(s.StaffCard.Length - 7) == password).ToList();
+            List<Staff> list = BLL.GetList().Where(s => s.StaffName == username && s.StaffCard.Substring(s.StaffCard.Length - 7) == password).ToList();
             if (username=="卫宇航"&&password=="327614")
             {
-                return View("<script>alert('成功!')</script>");
+                Response.Write("<script>alert('成功!')</script>");
             }
             return View();
         }
