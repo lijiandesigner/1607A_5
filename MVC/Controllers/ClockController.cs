8c5b35e627576c2f32e6eaf168598c2dff26eaf5
张野 <1339274045@qq.com>
2019-01-05 11:32:18 +0800

    kaoqin

---
+++ MVC/Controllers/ClockController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using BLL;
+namespace MVC.Controllers
+{
+    public class ClockController : Controller
+    {
+        // GET: Clock
+        ClockBll bll = new ClockBll();
+        public ActionResult Index(string FindBH,string FindName)
+        {
+            List<Clock> list = bll.GetList();
+            var linq = from a in list select a;
+            if(FindBH!="")
+            {
+                linq = from a in list where a.StaffNO.Contains(FindBH) select a;
+            }
+            if(FindName!="")
+            {
+                linq = from a in list where a.StaffName.Contains(FindName) select a;
+            }
+            if(FindBH!=""&&FindName!="")
+            {
+                linq = from a in list where a.StaffName.Contains(FindName) && a.StaffNO.Contains(FindBH) select a;
+            }
+            return View(linq);
+        } 
+        [HttpGet]
+        public ActionResult daka()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult daka(string userno,string password)
+        {
+            Clock clock = new Clock();
+            StaffBLL staffBLL = new StaffBLL();
+            ClockBll clockBll = new ClockBll();
+            if (userno == "0916545" && password == "0916545")
+            {
+                clock.StaffNO = userno;
+                clock.StaffName = "";
+                //clock.StaffName = staffBLL.GetList().Where(m => m.StaffNo == userno).FirstOrDefault().StaffName;
+                clock.HitTime = DateTime.Now;
+                if (clock.HitTime.ToString().CompareTo(DateTime.Now.ToString("yyyy/MM/dd") + "19:00") == -1)
+                {
+                    clock.HitSate = "上午上班打卡成功";
+                }
+                if (clockBll.Add(clock) > 1)
+                {
+                    Response.Write("<script>alert('打卡成功');location.href='/Clock/Index'</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('打卡失败')</script>");
+            }
+            return View();
+        }
+        public ActionResult Del(int id)
+        {
+            if(bll.Del(id)>0)
+            {
+                Response.Write("<script>alert('删除成功');location.href='/Clock/Index'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('删除失败')</script>");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Upd(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upd(Clock c)
+        {
+            if(bll.Upt(c)>0)
+            {
+                Response.Write("<script>alert('修改成功');location.href='/Clock/Index'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('修改失败')</script>");
+            }
+            return View();
+        }
+    }
+}
--- MVC/Controllers/GoTimeController.cs
+++ MVC/Controllers/GoTimeController.cs
@@ -22,19 +22,48 @@ namespace MVC.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult add(GoTime g)
+        public ActionResult add(string amH,string amM,string comH,string comM,string pmH,string pmM,string comP,string comPP)
         {
+            GoTime g = new GoTime();
+            var p = DateTime.Now.ToString("yyyy/MM/dd");
+            g.AMGoTime = Convert.ToDateTime(p +" "+ amH + ":" + amM);
+            g.AMComeTime = Convert.ToDateTime(p + " " + comH + ":" + comM);
+            g.PMGoTime = Convert.ToDateTime(p + " " + pmH + ":" + pmM);
+            g.PMComeTime = Convert.ToDateTime(p + " " + comP + ":" + comPP);
             int q = bll.Add(g);
-            if(q>0)
+            if (q > 0)
             {
-                Response.Write("<script>alert('打卡成功');location.href='/GoTime/Index';</script>");
+                Response.Write("<script>alert('添加成功');location.href='/GoTime/Index';</script>");
             }
             else
             {
-                return Content("失败");
+                Response.Write("<script>alert('添加失败')</script>");
             }
             return View();
         }
-        
+        [HttpGet]
+        public ActionResult Upd(int id)
+        {
+            return View(bll.GetT(id));
+        }
+        [HttpPost]
+        public ActionResult Upd(string amH, string amM, string comH, string comM, string pmH, string pmM, string comP, string comPP)
+        {
+            GoTime g = new GoTime();
+            var p = DateTime.Now.ToString("yyyy/MM/dd");
+            g.AMGoTime = Convert.ToDateTime(p + " " + amH + ":" + amM);
+            g.AMComeTime = Convert.ToDateTime(p + " " + comH + ":" + comM);
+            g.PMGoTime = Convert.ToDateTime(p + " " + pmH + ":" + pmM);
+            g.PMComeTime = Convert.ToDateTime(p + " " + comP + ":" + comPP);
+            if (bll.Upt(g)>0)
+            {
+                Response.Write("<script>alert('修改成功');location.href='/GoTime/Index';</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('修改失败')</script>");
+            }
+            return View();
+        }
     }
 }
